0b7db7ad7f26421ade12d0b93f680c61e713e506
Sachin <sachinlakshan04@gmail.com>
2023-09-04 19:06:42 +0530

    Update blog post

--- main.py
+++ main.py
@@ -50,3 +50,13 @@ def delete_post_by_id(id: int, db: Session = Depends(get_db)):
                                  id).delete(synchronize_session=False)
     db.commit()
     return {"mesg": f'Blog Post with id: {id} deleted successfully.'}
+
+
+@app.put('/posts/{id}')
+def update_post_by_id(id: int, request: schemas.Post, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if not post.first():
+        pass
+    post.update(request.model_dump())
+    db.commit()
+    return {"mesg": f'Blog Post with id: {id} successfully updated.'}
