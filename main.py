1d4ccb1ae310972978e69c4a70f987001612ad40
Sachin <sachinlakshan04@gmail.com>
2023-09-04 17:52:41 +0530

    create a post sqlalchemy model

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from schemas import Post
+from database import engine
+import models
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
 
 @app.post('/posts')
 def create_post(post: Post):
